ba14902fd390634ec992f525749590b2acb608e1
kr-g <k.r.goger+github@gmail.com>
2020-04-02 11:50:00 +0200

    added pttydev impl

--- mpycntrl/__main__.py
+++ mpycntrl/__main__.py
@@ -1,7 +1,11 @@
 import time
+import argparse
+
 import serial
 
-from mpycntrl import *
+from mpycntrl import MPyControl
+from pttydev import pttyopen, pttywsopen, PseudoTTY
+
 
 def print_tr( tr, l, head="received", debug=True ):
     if tr:
@@ -17,7 +21,25 @@ def print_tr( tr, l, head="received", debug=True ):
         print( head, l )
     else:
         print( l )
-        
+    
+    
+args = None
+
+
+def get_pttyopen():
+
+    return pttyopen(port=args.port, baudrate=args.baud,
+                    bytesize=args.bytesize, parity=args.parity,
+                    stopbits=args.stopbits, timeout=args.timeout)
+
+
+def get_pttywsopen():
+    # check this !!!
+    # ESP32 delay 0.3-0.5
+    # ESP8266 delay 0.01
+    return pttywsopen( f"ws://{args.host_ip}:{args.host_port}",
+                       args.host_passwd, write_delay=args.host_delay )
+
 
 def main():
     
@@ -34,7 +56,6 @@ def main():
     stopbits = 1
     timeout = .35
 
-    import argparse
     parser = argparse.ArgumentParser(prog='mpycntrl', usage='python3 -m %(prog)s [DEVICE-PARAMETER] [options]',
                                      description='Control MicroPython via cmd line')
     parser.add_argument("-v", "--version", dest='show_version', action="store_true",
@@ -45,8 +66,10 @@ def main():
                         help="ip adress to use (default: %(default)s)" )
     parsergroupws.add_argument("-hostport", "-ipport", "-ipp", type=str, dest='host_port', action="store", metavar="PORT",                               
                         help="ip port to use (default: %(default)s)", default=8266 )
-    parsergroupws.add_argument("-passwd", "-pass", "-pwd", type=str, dest='host_pass', action="store", metavar="PASSWD",
-                        help="password to use (default: %(default)s)", default=123456 )
+    parsergroupws.add_argument("-passwd", "-pass", "-pwd", type=str, dest='host_passwd', action="store", metavar="PASSWD",
+                        help="password to use (default: %(default)s)", default="123456" )
+    parsergroupws.add_argument("-ipdelay", type=float, dest='host_delay', action="store", metavar="DELAY",
+                        help="delay for writing 255 byte blocks (default: %(default)s)", default=0.5 )
 
     parsergrouptty = parser.add_argument_group("TTY", "serial parameter" )
     parsergrouptty.add_argument("-port", "-p", type=str, dest='port', action="store",
@@ -62,10 +85,12 @@ def main():
     
     parser.add_argument("-timeout", "-to", type=float, dest='timeout', action="store",
                         help="timeout in sec to use (default: %(default)s)", default=timeout )
+    
     parser.add_argument("-trace", "-t", dest='trace', action="store_true",
                         help="display trace info (default: %(default)s)", default=trace )
     parser.add_argument("-debug", "-d", dest='debug', action="store_true",
                         help="display debug info (default: %(default)s)", default=debug )
+    
     parser.add_argument("-showtime", dest='show_time', action="store_true",
                         help="display execution time (default: %(default)s)", default=show_time )
     parser.add_argument("-follow", "-f", dest='follow', action="store_true",
@@ -75,6 +100,9 @@ def main():
 
     parser.add_argument("-blocksize", "-bs", type=int, dest='blocksize', action="store",
                         help="blocksize during file transfer (default: %(default)s)", default=512 )
+
+    parser.add_argument("-reconnect", type=int, dest='reconnect_after', action="store",
+                        help="reconnect delay when connection breaks (default: %(default)s)", default=1 )
 
     parser.add_argument("-translate", "-tr", dest='translate', action="store_true",
                         help="translate response as string where possible (default: %(default)s)", default=False )
@@ -107,6 +135,7 @@ def main():
     group.add_argument("-hash", dest='hash_file', metavar="FILE", action="store",
                        type=str, nargs=1, help="get hash value for a file" )
 
+    global args
     args = parser.parse_args()
     
     if args.debug:
@@ -118,14 +147,27 @@ def main():
 
     start_time = time.time()
     
-    if args.host_ip != None:
-        raise Exception("ptty device not yet implemented")
+    open_func = get_pttyopen if args.host_ip == None else get_pttywsopen
 
-    with serial.Serial(port=args.port, baudrate=args.baud,
-                       bytesize=args.bytesize, parity=args.parity, stopbits=args.stopbits,
-                       timeout=args.timeout) as ser:
+    tty = PseudoTTY(
+                    open_func,
+                                        
+                    timeout = args.timeout,
+                    block_size = args.blocksize,
+                    reconnect_delay = args.reconnect_after,
+                    
+                    debug = args.debug,
+                    trace_on = args.trace,
+                    
+                    thrd_debug = args.debug,
+                    thrd_trace_on = args.trace
+                    )
 
-        mpyc = MPyControl(ser,debug=args.debug,trace=args.trace)
+    with tty.open() as octx:
+        
+        octx.waitready()
+        
+        mpyc = MPyControl(octx,debug=args.debug,trace=args.trace)
         
         r = None
         
@@ -187,7 +229,7 @@ def main():
                 print_tr( args.translate, r, debug=args.debug )       
 
         while args.follow:
-            r = ser.readlines()        
+            r = octx.readlines()        
             for l in r:
                 print_tr( args.translate, l, debug=args.debug )
             
@@ -200,8 +242,8 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        pass
-    except Exception as ex:
-        print( "error", ex )
+        print("cntrl+c")
+#    except Exception as ex:
+#        print( "error", ex )
 
 
